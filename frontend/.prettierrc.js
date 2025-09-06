528d273d0d7438f78d51ca59040225d8761983bb
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2025-09-06 01:00:44 -0700

    Init

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
+
+export default config;
---
+++ frontend/eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintReact from "@eslint-react/eslint-plugin"
+import tseslint from 'typescript-eslint'
+import { globalIgnores, defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      eslintReact.configs['recommended-typescript'],
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+])
